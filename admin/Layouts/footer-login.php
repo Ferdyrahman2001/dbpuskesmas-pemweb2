b6edd0a24b8ef35f9e6e3a1433d293a9eba9a19c
rahman.belajar <33442372+Ferdyrahman2001@users.noreply.github.com>
2025-05-14 06:35:15 +0700

    rsitdwebin

---
+++ admin/Layouts/footer-login.php
@@ -0,0 +1,7 @@
+					<footer>
+						<button type="submit" class="btn btn-primary pull-right">Sign In</button>
+						<div class="forgot-password pull-left">
+							<a href="page-password.html">Forgot password?</a> <br />
+							<a href="page-register.html"><b>Need to Register?</b></a>
+						</div>
+					</footer>
